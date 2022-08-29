8c612896b353adcf8849462fbe5ecf73fac5cdba
nizarrahman <102470420+nizarrahman@users.noreply.github.com>
2022-08-29 16:19:01 +0700

    Update ini-scbot.js

--- plugins/ini-scbot.js
+++ plugins/ini-scbot.js
@@ -3,7 +3,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = `*👤SAYA NYARI ESCE CUMA DI GITHUB BANH*\n\n_Silahkan Follow Github Saya Di Bawah ,Terimakasih_`
-conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/MendingTuru/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/nizarrahman/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
 ], m)
 }
